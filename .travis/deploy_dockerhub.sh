cf36c6cca344699226069331c1506047ec6fb2e0
Vlad <vladyslav.balabanov@gmail.com>
2022-12-21 23:01:03 +0100

    Edited dep

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -5,6 +5,8 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
 else
     TAG="$TRAVIS_BRANCH"
 fi
+echo $TRAVIS_REPO_SLUG
+
 docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
 docker tag $TRAVIS_REPO_SLUG $DOCKER_USER/$TRAVIS_REPO_SLUG
 docker push $DOCKER_USER/$TRAVIS_REPO_SLUG:$TAG
